8768f01a1e92e821cfea92ca423b0ed5de9408d9
imgesucakmak <cakmakimgesu@gmail.com>
2024-11-03 16:39:38 +0300

    Update StudentTest.java

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -1,30 +1,56 @@
 import java.util.Scanner;
 
-
 public class StudentTest {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
 
-       
         System.out.print("Adınızı girin: ");
-        String ad = scanner.nextLine();
-        
+        String ad1 = scanner.nextLine();
+
         System.out.print("Soyadınızı girin: ");
-        String soyad = scanner.nextLine();
+        String soyad1 = scanner.nextLine();
 
-        System.out.print("Ders ortalamanızı girin: ");
-        int not = scanner.nextInt();
+        int not1;
+        while (true) {
+            System.out.print("Ders ortalamanızı girin (0-100 arası): ");
+            not1 = scanner.nextInt();
+            if (not1 >= 0 && not1 <= 100) {
+                break;
+            } else {
+                System.out.println("Geçersiz not. Lütfen 0-100 aralığında bir not girin.");
+            }
+        }
+
+        Student student1 = new Student(ad1, soyad1);
+        student1.setNot(not1);
+
         
-        Student student1= new Student(ad, soyad,not);
+        scanner.nextLine(); 
+
+        System.out.print("Adınızı girin: ");
+        String ad2 = scanner.nextLine();
+
+        System.out.print("Soyadınızı girin: ");
+        String soyad2 = scanner.nextLine();
+
+        int not2;
+        while (true) {
+            System.out.print("Ders ortalamanızı girin (0-100 arası): ");
+            not2 = scanner.nextInt();
+            if (not2 >= 0 && not2 <= 100) {
+                break;
+            } else {
+                System.out.println("Geçersiz not. Lütfen 0-100 aralığında bir not girin.");
+            }
+        }
+
+        Student student2 = new Student(ad2, soyad2);
+        student2.setNot(not2);
+
+        
         student1.show();
-        
+        student2.show();
 
-        
-      
-        
-       
-	
-
-}
+        scanner.close();
+    }
 }
