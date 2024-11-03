0912492539a4c3ffbc7916354c8229e3c9dca475
imgesucakmak <cakmakimgesu@gmail.com>
2024-11-03 16:38:57 +0300

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,68 +1,39 @@
-import java.util.Scanner;
 public class Student {
-	public String ad;
+    public String ad;
     public String soyad;
-    public int not;
+    public int not = -1; // Başlangıç değeri olarak geçersiz bir not
 
-    public Student(String ad, String soyad, int not) {
+    public Student(String ad, String soyad) {
         this.ad = ad;
         this.soyad = soyad;
-        this.not =not;
-    }
-    
-    public void setAd(String ad) {
-        this.ad= ad;
     }
 
-    public void setSoyad(String soyad) {
-        this.soyad = soyad;
-    }
-
-    public void setdersOrtalama(int not) {
-        this.not = not;
-    }
-
-   
-    public String getAd() {
-        return ad;
-    }
-
-    public String getSoyad() {
-        return soyad;
-    }
-
-    public int getNot() {
-        return not;
-    }
-    
-    public String getHarfNotu() {
-    	if (not >= 90 && not <= 100) {
-            return "A";
-        } else if (not >= 80 && not < 90) {
-            return "B";
-        } else if (not >= 70 && not < 80) {
-            return "C";
-        } else if (not >= 60 && not < 70) {
-            return "D";
-        } else if (not >= 0 && not < 60) {
-            return "F";
+    public void setNot(int not) {
+        if (not < 0 || not > 100) {
+            System.out.println("Geçerli not girin!");
+            this.not = -1; // Geçersiz not
         } else {
-            return "Geçerli bir not girin";
+            this.not = not;
         }
-    	
     }
-    
+
+    public String getHarfNotu() {
+        if (not < 0 || not > 100) {
+            return "Geçersiz not, harf notu hesaplanamadı.";
+        }
+        if (not >= 90) return "A";
+        else if (not >= 80) return "B";
+        else if (not >= 70) return "C";
+        else if (not >= 60) return "D";
+        else return "F";
+    }
+
     public void show() {
         System.out.println("Ad Soyad: " + ad + " " + soyad);
-        System.out.println("Ders Ortalaması: " + not);
-        System.out.println("Harf Notu: " + getHarfNotu());}
-       
-    
-
-
-    
-    
-
-       
-
+        if (not >= 0 && not <= 100) {
+            System.out.println("Harf Notu: " + getHarfNotu());
+        } else {
+            System.out.println("Geçersiz not girildi.");
+        }
+    }
 }
